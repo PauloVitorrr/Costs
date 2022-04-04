441e386d7c367813a755883583534b21d5b787ec
Paulo Vitor Sousa <paulovitorvaztst@gmail.com>
2022-04-04 18:27:35 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
 import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import NewProject from './components/pages/NewProject'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
-import NewProject from './components/pages/NewProject'
-
-
-import Container from './components/layout/Container'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import Projects from './components/pages/Projects' 
+import Project from './components/pages/Project'
 
 function App() {
   return (
@@ -20,7 +19,8 @@ function App() {
             <Route path= "/Projects" element={<Projects/>}/>
             <Route path= "/Company" element={<Company/>}/> 
             <Route path= "/Contact" element={<Contact/>}/> 
-            <Route path= "/NewProject" element={<NewProject/>}/>    
+            <Route path= "/NewProject" element={<NewProject/>}/>  
+            <Route path= "/Project/:id" element={<Project/>}/>      
           </Routes>
         </Container>
       <Footer />
